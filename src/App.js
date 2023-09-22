487fb6f6ab6cc708e5a7687ef54ef52c3a00ec8b
DiegoAle6 <diegoalemanmena@gmail.com>
2023-09-22 08:23:10 -0500

    Cambios Diego

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CondicionalAtmosferica from './Componentes/CondicionalAtmosferica';
+
+function App() {
+  return (
+    
+    <div className='App'>
+      <CondicionalAtmosferica/>
+    </div>
+    
+  );
+}
+
+export default App;
