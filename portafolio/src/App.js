6bb9d11d1089ffc9e5ef35d8be1b2fb6d5b1aeff
ricardojch.12@gmail.com <ricardojch.12@gmail.com>
2023-09-22 01:04:06 -0500

    Cambios Richi

---
+++ portafolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CondicionalAtmosferica from './Componentes/CondicionalAtmosferica';
+
+function App() {
+  return (
+    
+    <div>
+      <CondicionalAtmosferica/>
+    </div>
+    
+  );
+}
+
+export default App;
